ea040c1ae2ba3466e16e9b99263ea10ad9ce81f7
Sambhav Jain <sambhav.jain@amd.com>
2025-08-21 21:27:02 +0000

    cleanup hip test, fix build warnings

--- sharkfuser/tests/hip_tests/test_hip_buffer_import_api.cpp
+++ sharkfuser/tests/hip_tests/test_hip_buffer_import_api.cpp
@@ -21,38 +21,55 @@ __global__ void hello_kernel() {
   printf("Hello from GPU! block %d thread %d\n", blockIdx.x, threadIdx.x);
 }
 
+// Utility macro to check status of HIP functions that are set with nodiscard
+#define HIP_REQUIRE_SUCCESS(expr)                                              \
+  ({                                                                           \
+    auto err = (expr);                                                         \
+    if (err != hipSuccess) {                                                   \
+      fprintf(stderr, "Error: %s\n", hipGetErrorString(err));                  \
+    }                                                                          \
+    REQUIRE(err == hipSuccess);                                                \
+  })
+
 TEST_CASE("proof of life for HIP", "[hip_tests]") {
   // ----------------------------------------------------------------------
   //  proof of life for GPU connection
   // ----------------------------------------------------------------------
 
   int dev = 0;
+  HIP_REQUIRE_SUCCESS(hipGetDevice(&dev));
+
   hipDeviceProp_t prop{};
-  hipGetDevice(&dev);
-  hipGetDeviceProperties(&prop, dev);
+  HIP_REQUIRE_SUCCESS(hipGetDeviceProperties(&prop, dev));
 
   void *ptr;
-  hipMalloc(&ptr, sizeof(float) * 64);
+  HIP_REQUIRE_SUCCESS(hipMalloc(&ptr, sizeof(float) * 64));
 
   // Launch kernel (1 block, 4 threads)
   hipLaunchKernelGGL(hello_kernel, dim3(1), dim3(4), 0, 0);
-  hipError_t err = hipDeviceSynchronize();
-  if (err != hipSuccess) {
-    fprintf(stderr, "hipDeviceSynchronize: %s\n", hipGetErrorString(err));
-  }
-  REQUIRE(err == hipSuccess);
+
+  HIP_REQUIRE_SUCCESS(hipDeviceSynchronize());
 }
 
 TEST_CASE("Buffer import", "[hip_tests]") {
+  // --------------------------------------------------------------------------
+  //  Test for the externally managed buffers imported as IREE HAL Buffer View
+  //
+  //  `hipMalloc`'ed `void*` -> `iree_hal_buffer_view` (via `import_buffer`)
+  // --------------------------------------------------------------------------
+
+  // Pointer to hipMalloc'ed buffer on device
+  void *ptr;
+  HIP_REQUIRE_SUCCESS(hipMalloc(&ptr, sizeof(float) * 64));
 
   Graph graph;
   graph.setBackend(Backend::GFX942);
 
-  iree_hal_allocator_t *device_allocator =
+  // Get device allocator
+  iree_hal_allocator_t *allocator =
       iree_hal_device_allocator(FUSILLI_REQUIRE_UNWRAP(graph.getDevice()));
 
-  void *ptr;
-  hipMalloc(&ptr, sizeof(float) * 64);
+  // Create external buffer wrapping the void*
   iree_hal_external_buffer_t external_buffer = {
       .type = IREE_HAL_EXTERNAL_BUFFER_TYPE_DEVICE_ALLOCATION,
       .flags = 0,
@@ -66,15 +83,22 @@ TEST_CASE("Buffer import", "[hip_tests]") {
           },
   };
 
-  iree_hal_buffer_params_t buffer_params = {
-      .type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
-      .usage = IREE_HAL_BUFFER_USAGE_TRANSFER |
-               IREE_HAL_BUFFER_USAGE_DISPATCH_STORAGE,
-  };
-  iree_hal_buffer_t *imported_buffer = NULL;
+  // Set buffer parameters
+  iree_hal_buffer_params_t params = {0};
+  // Where to allocate (host or device)
+  params.type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL;
+  // Access to allow to this memory
+  params.access = IREE_HAL_MEMORY_ACCESS_ALL;
+  // Intended usage of the buffer (transfers, dispatches, etc)
+  params.usage = IREE_HAL_BUFFER_USAGE_DEFAULT;
+
+  // Create imported buffer to be populated with externally managed buffer
+  iree_hal_buffer_t *out_buffer = NULL;
+
   iree_hal_buffer_release_callback_t release_callback =
       iree_hal_buffer_release_callback_null();
+
+  // Import buffer
   REQUIRE(isOk(iree_hal_allocator_import_buffer(
-      device_allocator, buffer_params, &external_buffer, release_callback,
-      &imported_buffer)));
+      allocator, params, &external_buffer, release_callback, &out_buffer)));
 }
